19b01d6fcfa64a40199e1cdf752fbe137bd61209
Hagay Haut <hagayhaut@gmail.com>
2023-01-05 22:00:27 -0500

    cors config, add bootstrap

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Dating App';
+  users: any;
+
+  constructor(private _http: HttpClient) {}
+
+  ngOnInit(): void {
+    this._http.get('http://localhost:5000/api/users')
+      .subscribe({
+        next: (response) => this.users = response,
+        error: (error) => console.error(error),
+        complete: () => console.log('Request has completed'),
+      })
+  }
+
+  
 }
