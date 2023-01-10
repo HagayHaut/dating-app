372df6977dfc083bc779dbd9a3d7eccd4bd5882f
Hagay Haut <hagayhaut@gmail.com>
2023-01-09 20:24:13 -0500

    upd: DOB column to DateTime, including extensions

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -8,7 +8,7 @@ namespace API.Entities
         public string UserName { get; set; }
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
-        public DateOnly DateOfBirth { get; set; }
+        public DateTime DateOfBirth { get; set; }
         public string KnownAs { get; set; }
         public DateTime Created { get; set; } = DateTime.UtcNow;
         public DateTime LastActive { get; set; } = DateTime.UtcNow;
--- API/Extensions/DateTimeExtensions.cs
+++ API/Extensions/DateTimeExtensions.cs
@@ -2,9 +2,9 @@ namespace API.Extensions
 {
     public static class DateTimeExtensions
     {
-        public static int CalculateAge(this DateOnly dob)
+        public static int CalculateAge(this DateTime dob)
         {
-            var today = DateOnly.FromDateTime(DateTime.UtcNow);
+            var today = DateTime.UtcNow;
 
             var age = today.Year - dob.Year;
 
