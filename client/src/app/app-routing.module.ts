bf6e159db4e9271cf98049f155d7b5c69425ab29
Hagay Haut <hagayhaut@gmail.com>
2023-01-13 21:26:53 -0500

    routing params to users page

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -25,7 +25,7 @@ const routes: Routes = [
         component: MembersListComponent,
       },
       {
-        path: 'members/:id',
+        path: 'members/:username',
         component: MembersDetailComponent,
       },
       {
--- client/src/app/members/members-detail/members-detail.component.ts
+++ client/src/app/members/members-detail/members-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Member } from 'src/app/models/member';
+import { MembersService } from 'src/app/_services/members.service';
 
 @Component({
   selector: 'app-members-detail',
@@ -6,10 +9,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./members-detail.component.css']
 })
 export class MembersDetailComponent implements OnInit {
+  member: Member | undefined;
 
-  constructor() { }
+  constructor(private _memberService: MembersService, private _route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.loadMember();
   }
 
+  loadMember() {
+    const username = this._route.snapshot.paramMap.get('username');
+    if (!username) return;
+    this._memberService.getMember(username).subscribe({
+      next: member => this.member = member,
+    })
+  }
 }
